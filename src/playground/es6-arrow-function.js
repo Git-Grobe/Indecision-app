ace23758181e43de56b7f4cc85a0b6626241c71c
Scott Grobe <gitgrobe@gmail.com>
2019-01-01 14:40:59 -0500

    ES6 aside: arrow functions lesson demo code

--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -1,7 +1,5 @@
-'use strict';
-
 //ES5 functions
-var square = function square(x) {
+const square = function (x) {
     return x * x;
 };
 
@@ -10,19 +8,30 @@ function square2(x) {
     return x * x;
 }
 
+
+
+
+
+
 //ES6 functions
-var squareArrow = function squareArrow(x) {
+const squareArrow = (x) => {
     return x * x;
 };
 
 //concise ES6 expression syntax, implicitly returning one thing
-var squareArrow2 = function squareArrow2(x) {
-    return x * x;
-};
+const squareArrow2 = (x) => x * x;
+
+
+
+
+
+
 
 //ES5
 console.log(square(9));
 console.log(square2(9));
+
+
 
 //ES6 arrow function
 console.log(squareArrow(8));
@@ -30,10 +39,10 @@ console.log(squareArrow(8));
 //ES6 concise arrow function
 console.log(squareArrow2(4));
 
+
+
 //coding challenge, create function that returns just the first name of string
 
-var getFirstName = function getFirstName(fullName) {
-    return fullName.split(' ')[0];
-};
+const getFirstName = (fullName) => fullName.split(' ')[0];
 
 console.log(getFirstName('First Last'));
