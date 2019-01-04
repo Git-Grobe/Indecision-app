67e1ded6ecb5b9f9d23dbe29fc4eaa6210cc326d
Scott Grobe <gitgrobe@gmail.com>
2019-01-03 21:37:27 -0500

    ES6: arrow functions part two, using map and concise return values

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,5 @@
-"use strict";
+'use strict';
 
-var _arguments = arguments;
 // arguments object - no longer bound with arrow functions
 
 var add = function add(a, b) {
@@ -10,9 +9,64 @@ var add = function add(a, b) {
 console.log(add(55, 1, 1001));
 
 var addArrow = function addArrow(a, b) {
-    console.log(_arguments); //no longer have access to arguments
+    // console.log(arguments); //no longer have access to arguments
     return a + b;
 };
 console.log(addArrow(100, 1, 1001));
 
-// this keyword - no longer bound with arrow functions
+// this keyword - no longer bound
+
+
+//es5
+
+// const user = {
+//     name: 'Scott',
+//     cities: ['Durham', 'Philadelphia', 'Raleigh'],
+//     printPlacesLived: function () {
+//         console.log(this.name);
+//         console.log(this.cities);
+
+//         const that = this;
+
+//         this.cities.forEach(function (city) {
+//             console.log(that.name + ' has lived in ' + city);
+//         });
+//     }
+// };
+
+// user.printPlacesLived();
+
+
+//arrow function uses parents this value
+var user = {
+    name: 'Scott',
+    cities: ['Durham', 'Philadelphia', 'Raleigh'],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
+
+        return this.cities.map(function (city) {
+            return _this.name + ' has loved in ' + city;
+        });
+    }
+};
+
+console.log(user.printPlacesLived());
+
+// Challenge 
+// numbers - array of numbers
+// multiplyBy - single number
+// multiply - return a new array where the numbers have been multiplied
+
+var multiplier = {
+    numbers: [2, 4, 6],
+    multiplyBy: 10,
+    multiply: function multiply() {
+        var _this2 = this;
+
+        return this.numbers.map(function (number) {
+            return number * _this2.multiplyBy;
+        });
+    }
+};
+
+console.log(multiplier.multiply());
--- src/playground/es6-arrow-function2.js
+++ src/playground/es6-arrow-function2.js
@@ -7,9 +7,62 @@ const add = function (a, b) {
 console.log(add(55, 1, 1001));
 
 const addArrow = (a,b) => {
-    console.log(arguments); //no longer have access to arguments
+    // console.log(arguments); //no longer have access to arguments
     return a + b;
 };
 console.log(addArrow(100, 1, 1001));
 
-// this keyword - no longer bound with arrow functions
+// this keyword - no longer bound
+
+
+//es5
+
+// const user = {
+//     name: 'Scott',
+//     cities: ['Durham', 'Philadelphia', 'Raleigh'],
+//     printPlacesLived: function () {
+//         console.log(this.name);
+//         console.log(this.cities);
+
+//         const that = this;
+
+//         this.cities.forEach(function (city) {
+//             console.log(that.name + ' has lived in ' + city);
+//         });
+//     }
+// };
+
+// user.printPlacesLived();
+
+
+
+
+//arrow function uses parents this value
+const user = {
+    name: 'Scott',
+    cities: ['Durham', 'Philadelphia', 'Raleigh'],
+    printPlacesLived() {
+        return this.cities.map((city) => this.name + ' has loved in ' + city);
+    }
+};
+
+console.log(user.printPlacesLived());
+
+
+
+
+
+// Challenge 
+// numbers - array of numbers
+// multiplyBy - single number
+// multiply - return a new array where the numbers have been multiplied
+
+const multiplier = {
+    numbers: [2, 4, 6],
+    multiplyBy: 10,
+    multiply() {
+        return this.numbers.map((number) => number * this.multiplyBy)
+    }
+};
+
+console.log(multiplier.multiply());
